f4ff4a77762eada31036cd6fd9431afa672a1868
Hector <141383937+hfelix3@users.noreply.github.com>
2023-10-09 08:26:37 -0700

    search key function added

--- Assets/js/api1.js
+++ Assets/js/api1.js
@@ -139,12 +139,17 @@ $('#searchBtn').on('click', function(event) {
 
 });
 
+//SEARCH WITH ENTER KEY FUNCTION
+// Get the input field
+var input = document.getElementById("searchTerms");
 
-
-
-
-
-
-
-
-
+// Execute a function when the user presses a key on the keyboard
+input.addEventListener("keypress", function(event) {
+  // If the user presses the "Enter" key on the keyboard
+  if (event.key === "Enter") {
+    // Cancel the default action, if needed
+    event.preventDefault();
+    // Trigger the button element with a click
+    document.getElementById("searchBtn").click();
+  }
+});
