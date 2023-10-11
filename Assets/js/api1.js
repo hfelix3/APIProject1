177ad8593f9892f3c56178ab3f93a24bd32e3ec4
= <the.digital.avatar@gmail.com>
2023-10-10 20:50:43 -0700

    Made some cleanup changes

--- Assets/js/api1.js
+++ Assets/js/api1.js
@@ -1,13 +1,9 @@
-
-
 function runSearch() {
   var searchTerm = $('#searchTerms').val();
   localStorage.setItem("searchTerm", searchTerm);
   searchTerm = searchTerm.replace(/ /g, '+');
   
-
-
-  //FOR REFACTOR, EXTRACT THIS AS ITS OWN FUNCTION
+  //FOR REFACTOR, EXTRACT THIS AS ITS OWN FUNCTION?
   var url = "https://openlibrary.org/search.json?title=" + searchTerm;
   console.log(url);  //TODO REMOVE CONSOLE LOG
 
@@ -28,19 +24,13 @@ function runSearch() {
             }
           })
 
-
-
         function createBookList(books) {
           var searchResults = $("#searchresults");
           searchResults.empty();
-          
           //FOORLOOP FOR SEARCH RESULTS
           $.each(books, async function(index, book) { 
-
             try {
-
             var bookDiv = $("<div class='p-2 w-48 h-99 overflow-hidden bg-slate-100 hover:bg-slate-200 flex-row border border-gray-300 rounded-lg shadow'>");
-
             var bookTitle = book.title;
             var bookAuthor = book.author_name[0];
             var bookPublishedYear = book.publish_year[0];
@@ -49,9 +39,6 @@ function runSearch() {
             var bookCover = book.cover_i;
             var bookLanguage = book.language[0];
             var bookEbook = book.ebook_access;
-
-            
-            
             var bookCoverImg = $("<img>");
             var bookTitleH3 = $("<h3 class='underline flex flex-wrap'>");
             var bookAuthorP = $("<p>");
@@ -63,28 +50,19 @@ function runSearch() {
             var bookPrices = $("<a>");
             bookPrices.addClass("bookPrices");  //The API2 will pull the ISBNs later targeting the isbn stored in the purchase element
             bookPrices.attr("data-isbn", bookISBN);
-
-
-            //TODO: assign variable into elements
-            bookAuthorP.text("Author: " + bookAuthor + ".");
-            bookTitleH3.text("Title: " + bookTitle + ".");
-            bookPublishedYearP.text("Year published: " + bookPublishedYear + ".");
-            bookPublisherP.text("Publisher: " + bookPublisher + ".");
+            bookAuthorP.text("Author: " + bookAuthor);
+            bookTitleH3.text("Title: " + bookTitle);
+            bookPublishedYearP.text("Year published: " + bookPublishedYear);
+            bookPublisherP.text("Publisher: " + bookPublisher);
             bookISBNP.text("ISBN: " +bookISBN);
-            bookLanguageP.text("Language: " + bookLanguage + ".");
-            bookEbookP.text("E-book: " + bookEbook + ".");
+            bookLanguageP.text("Language: " + bookLanguage);
+            bookEbookP.text("E-book: " + bookEbook);
             bookPrices.text("Loading store offers...");
-            
-
             if (bookCover == null){
-              //console.log("notworking");
               bookCoverImg.attr("src", "https://www.press.uillinois.edu/books/images/no_cover.jpg");
             } else {
               bookCoverImg.attr("src", "https://covers.openlibrary.org/b/id/"+ bookCover +"-M.jpg");
-
             }
-            
-
             //append elements to book div
             bookCoverImg.appendTo(bookDiv);
             bookTitleH3.appendTo(bookDiv);
@@ -97,29 +75,18 @@ function runSearch() {
             bookPrices.appendTo(bookDiv);
             //final append finished, book div to search results
             bookDiv.appendTo(searchResults);
-
-
-         } catch (error) { //console.log(book, error);
-        }  
-            
-
-
-          }
-         
-
-          )
-          setISBNs(); //THIS IS WHERE API2 IS CALLED
-        };
-        
-
+         } catch (error) { console.log(book, error);
+          }  
+      });
+        setISBNs(); //THIS IS WHERE API2 IS CALLED
+    };
 };
-
 
 function setISBNs() {
   var isbnelements = document.querySelectorAll(".bookPrices");
   $.each(isbnelements, function(index, element) {
     var isbn = isbnelements[index].getAttribute("data-isbn");
-        var url = "http://betweengameactions.com/api.php?type=buy&isbn=" + isbn;
+        var url = "https://atlnettech.com/api.php?type=buy&isbn=" + isbn;
         $.ajax({
             url: url,
             type: 'GET',
@@ -140,48 +107,21 @@ function setISBNs() {
             console.log("error");
             }
           });
-    
   });
-
 }
 //SEARCH WITH ENTER KEY FUNCTION.
 // Get the input field
 var input = document.getElementById("searchTerms");
-
 // Execute a function when the user presses a key on the keyboard
 input.addEventListener("keypress", function(event) {
   // If the user presses the "Enter" key on the keyboard
   if (event.key === "Enter") {
-    // Cancel the default action, if needed
+    // Cancel the default action
     event.preventDefault();
     // Trigger the button element with a click
-    document.getElementById("searchBtn").click();
+    $("#searchBtn").click();
   }
 });
-
-
-
-
-var returnData = async function(isbn) {
-            
-            
-
-  var url = "http://betweengameactions.com/api.php?type=buy&isbn=" + isbn;
-  $.ajax({
-      url: url,
-      type: 'GET',
-      dataType: 'json',
-      success: async function(data) {
-       return data;           
-      },
-      error: function() {
-      console.log("error");
-      }
-      });
-
-      }
-      
-
 
 function setPrices(url, priceObject) {
   console.log(url);
@@ -194,7 +134,6 @@ function setPrices(url, priceObject) {
 return booksrunValues;
 };
 
-
 if(localStorage.getItem("searchTerm") != undefined) {
   $('#searchTerms').val( localStorage.getItem("searchTerm"));
   runSearch();
@@ -204,5 +143,3 @@ $('#searchBtn').on('click', function(event) {
   event.preventDefault();
   runSearch();
 });
-
-
